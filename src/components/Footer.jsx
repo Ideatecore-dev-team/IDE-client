1491c5a2528231b9107780e3f886583240d82968
nhsltn <nhsltndev@gmail.com>
2024-12-10 23:23:55 +0700

    FIX: Homepage responsive temporary done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,17 +12,17 @@ import { SiFacebook } from "react-icons/si";
 
 export const Footer = () => {
   return (
-    <footer className="footer flex px-6 py-[60px] flex-col content-center items-center gap-8 self-stretch bg-black text-white">
-      <div className="footer-container flex flex-col items-start gap-8 self-stretch">
-        <div className="logo-heading flex flex-col items-start gap-3 self-stretch">
+    <footer className="footer flex px-6 py-[60px] flex-col content-center items-center gap-8 xs:self-stretch bg-black text-white">
+      <div className="footer-container w-full lg:w-[1224px] flex flex-col items-start gap-8 xs:self-stretch">
+        <div className="logo-heading w-full lg:w-[440px] flex flex-col items-start gap-3 xs:self-stretch">
           <div className="logo-holder flex">
             <img src={FooterLogo} alt="" />
           </div>
-          <h2 className=" text-2xl font-bold text-white">
+          <h2 className=" text-2xl lg:text-[32px] font-bold text-white">
             Bersama Wujudkan Generasi Pemimpin Indonesia
           </h2>
         </div>
-        <div className="footer-details flex flex-col items-start gap-12 self-stretch">
+        <div className="footer-details flex flex-col lg:flex-row lg:justify-between items-start xs:gap-12 self-stretch">
           <div className="details flex flex-col items-start gap-4 w-56">
             <div className="detail flex items-center gap-3">
               <MdLocalPhone />
@@ -39,7 +39,36 @@ export const Footer = () => {
               <p className="text-base font-normal">Jakarta, Indonesia</p>
             </div>
           </div>
-          <div className="explore flex flex-col gap-4">
+          <div className="footer-menu xs:hidden flex items-start gap-[40px]">
+            <div className="explore flex flex-col gap-4">
+              <p className="text-sm font-bold">Explore</p>
+              <div className="footer-link flex flex-col items-start gap-6 self-stretch">
+                <Link className="text-base font-normal">Our Program</Link>
+                <Link className="text-base font-normal">Gallery</Link>
+                <Link className="text-base font-normal">Article</Link>
+                <Link className="text-base font-normal">FAQ</Link>
+              </div>
+            </div>
+            <div className="border-r-[1px] border-neutral-3 h-[192px] lg:block hidden"></div>
+            <div className="social flex flex-col items-start gap-3">
+              <p className="text-sm font-bold">Follow Us</p>
+              <div className="social-media flex items-center gap-3 self-stretch">
+                <Link className="flex justify-center items-center p-3 rounded-lg bg-brand-red">
+                  <SiLinkedin />
+                </Link>
+                <Link className="flex justify-center items-center p-3 rounded-lg bg-brand-red">
+                  <SiInstagram />
+                </Link>
+                <Link className="flex justify-center items-center p-3 rounded-lg bg-brand-red">
+                  <SiYoutube />
+                </Link>
+                <Link className="flex justify-center items-center p-3 rounded-lg bg-brand-red">
+                  <SiFacebook />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="explore lg:hidden flex flex-col gap-4">
             <p className="text-sm font-bold">Explore</p>
             <div className="footer-link flex flex-col items-start gap-6 self-stretch">
               <Link className="text-base font-normal">Our Program</Link>
@@ -48,7 +77,7 @@ export const Footer = () => {
               <Link className="text-base font-normal">FAQ</Link>
             </div>
           </div>
-          <div className="social flex flex-col items-start gap-3">
+          <div className="social lg:hidden flex flex-col items-start gap-3">
             <p className="text-sm font-bold">Follow Us</p>
             <div className="social-media flex items-center gap-3 self-stretch">
               <Link className="flex justify-center items-center p-3 rounded-lg bg-brand-red">
@@ -66,7 +95,7 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <hr className="border-b-[1px] w-96 flex-1 border-neutral-3" />
+        <hr className="border-b-[1px] w-96 lg:w-full flex-1 border-neutral-3" />
         <p className="text-sm font-bold">
           Copyright2024 IDE Indonesia. All rights reserved.
         </p>
