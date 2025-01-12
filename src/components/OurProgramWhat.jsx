29c5f9c97c07ede50d4197dcf4771cbed5c63d5d
nhsltn <nhsltndev@gmail.com>
2025-01-12 23:41:44 +0700

    INIT: Our Program

--- src/components/OurProgramWhat.jsx
+++ src/components/OurProgramWhat.jsx
@@ -1,31 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { MdArrowDropDown } from "react-icons/md";
+import { OurProgramDropdownContent } from "./OurProgramDropdownContent";
 
 export const OurProgramWhat = () => {
+  const [selectedProgram, setSelectedProgram] = useState("1"); // Default value set to "1"
+
+  const handleSelectionChange = (e) => {
+    setSelectedProgram(e.target.value);
+  };
+
   return (
     <div className="what-we-do-section flex justify-center items-start self-stretch ">
-      <div className="what-we-do-container flex w-full xs:px-6 py-12 flex-col items-start">
-        <div className="title flex flex-col items-start gap-4 self-stretch">
+      <div className="what-we-do-container flex lg:w-[1224px] w-full xs:px-6 py-12 flex-col items-start">
+        <div className="title flex lg:w-[520px] flex-col items-start gap-4 xs:self-stretch">
           <div className="title-desc flex flex-col gap-3 items-start self-stretch">
-            <h2 className="text-2xl font-bold">What We Do?</h2>
+            <h2 className="text-2xl lg:text-[32px] font-bold">What We Do?</h2>
             <p className="text-base">
               Pelajari lebih lanjut tentang kegiatan yang kami lakukan di setiap
               program.
             </p>
           </div>
-          <form className="w-full">
+          <div className="dropdown relative w-full">
             <select
-              id="countries"
-              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              className="flex p-3 items-start w-full rounded-md border-[1px] border-neutral-2 appearance-none"
+              onChange={handleSelectionChange}
+              value={selectedProgram}
             >
-              <option value="US">United States</option>
-              <option value="CA">Canada</option>
-              <option value="FR">France</option>
-              <option value="DE">Germany</option>
+              <option value="1">Democracy Affairs</option>
+              <option value="2">Education Affairs</option>
+              <option value="3">Social Affairs</option>
+              <option value="4">Entrepreneurship Affairs</option>
+              <option value="5">Interfaith Affairs</option>
             </select>
-          </form>
+            <MdArrowDropDown
+              className="absolute top-1/2 right-3 -translate-y-1/2 text-neutral-500 pointer-events-none"
+              size={24}
+            />
+          </div>
         </div>
+        <OurProgramDropdownContent programId={selectedProgram} />
       </div>
     </div>
   );
