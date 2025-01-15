538eda809308f2cffbbf85b1ff2eb187e0c59c1f
WeissCurry <Maulanasykari@gmail.com>
2025-01-16 03:16:16 +0700

    STYLE Home, HomeCrouselSwiper, hover:bg-brand-red-hover

--- src/components/HomeCarouselSwiper.jsx
+++ src/components/HomeCarouselSwiper.jsx
@@ -18,10 +18,10 @@ export const HomeCarouselSwiper = () => {
 
   return (
     <div className="w-full relative mb-6">
-      <button className="custom-prev flex p-[12px] justify-center items-center rounded-[40px] bg-[#C82121] absolute left-16 top-1/2 transform -translate-y-1/2 z-10">
+      <button className="custom-prev flex p-[12px] justify-center items-center rounded-[40px] bg-brand-red hover:bg-brand-red-hover absolute left-16 top-1/2 transform -translate-y-1/2 z-10">
         <MdKeyboardArrowLeft size={24} color="#fff" />
       </button>
-      <button className="custom-next flex p-[12px] justify-center items-center rounded-[40px] bg-[#C82121] absolute right-16 top-1/2 transform -translate-y-1/2 z-10">
+      <button className="custom-next flex p-[12px] justify-center items-center rounded-[40px] bg-brand-red hover:bg-brand-red-hover absolute right-16 top-1/2 transform -translate-y-1/2 z-10">
         <MdKeyboardArrowRight size={24} color="#fff" />
       </button>
       <Swiper
