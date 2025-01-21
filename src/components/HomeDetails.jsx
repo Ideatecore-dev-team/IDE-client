4fe9bce026d4d178af0a8719cb90017898f680a5
WeissCurry <Maulanasykari@gmail.com>
2025-01-21 14:30:59 +0700

    FIX : Home, remove animation

--- src/components/HomeDetails.jsx
+++ src/components/HomeDetails.jsx
@@ -1,104 +1,109 @@
-import React, { useEffect, useState } from "react";    
-import { Link } from "react-router-dom";    
-import { MdLocationOn } from "react-icons/md";    
-import { MdLocationCity } from "react-icons/md";    
-import { MdPeople } from "react-icons/md";    
-import { motion } from "framer-motion";    
+import React, { useEffect, useState } from "react";        
+import { Link } from "react-router-dom";        
+import { MdLocationOn, MdLocationCity, MdPeople } from "react-icons/md";        
+import { motion } from "framer-motion";        
   
-export const HomeDetails = () => {    
-  const [isVisible, setIsVisible] = useState(false);    
+export const HomeDetails = () => {        
+  const [isVisible, setIsVisible] = useState(false);        
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   
-  const handleScroll = () => {    
-    const rect = document.getElementById("home-details").getBoundingClientRect();    
-    if (rect.top < window.innerHeight && rect.bottom >= 0) {    
-      setIsVisible(true);    
-    }    
-  };    
+  const handleScroll = () => {        
+    const rect = document.getElementById("home-details").getBoundingClientRect();        
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {        
+      setIsVisible(true);        
+    }        
+  };        
   
-  useEffect(() => {    
-    window.addEventListener("scroll", handleScroll);    
-    return () => {    
-      window.removeEventListener("scroll", handleScroll);    
-    };    
-  }, []);    
+  useEffect(() => {        
+    window.addEventListener("scroll", handleScroll);        
+    const handleResize = () => {  
+      setIsMobile(window.innerWidth < 768); // Update mobile state on resize  
+    };  
+    window.addEventListener("resize", handleResize); // Listen for resize events  
   
-  return (    
-    <motion.div    
-      id="home-details"    
-      className="details-section flex justify-center items-center self-stretch"    
-      initial={{ opacity: 0, x: -1000 }} // Start from the left    
-      animate={isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: -1000 }} // Move to the original position when visible    
-      transition={{ duration: 1, ease: "easeInOut" }}    
-    >    
-      <div className="details-container flex lg:flex-row flex-col w-full xs:px-6 lg:w-[1224px] py-12 items-start lg:items-center lg:content-between border-b-[1px] border-neutral-3 gap-10 lg:gap-12">    
-        <div className="our-mission lg:w-[568px] flex flex-col items-start gap-4 xs:self-stretch">    
-          <h5 className=" text-sm lg:text-base font-bold">OUR MISSION</h5>    
-          <div className="heading-desc flex flex-col items-start gap-3 self-stretch">    
-            <h2 className="text-2xl lg:text-3xl font-bold text-neutral-1">    
-              Melahirkan Manusia Indonesia Berkualitas melalui Ilmu Pengetahuan dan Pengembangan Keterampilan    
-            </h2>    
-            <p className="text-base font-normal text-neutral-2">    
-              IDE Indonesia hadir sebagai jembatan bagi generasi muda di seluruh Indonesia dalam mewujudkan     
-              mimpi dan cita-cita mereka melalui pemberdayaan,     
-              kolaborasi, dan gotong-royong menghadirkan solusi inovatif bagi kemajuan bangsa dan negara.    
-            </p>    
-            <Link    
-              className="text-base font-bold underline decoration-solid text-neutral-1"    
-              to="/about"    
-            >    
-              Learn More    
-            </Link>    
-          </div>    
-        </div>    
-        <hr className="w-full lg:hidden" />    
-        <div className="border-r-[1px] border-neutral-3 h-[402px] lg:block hidden"></div>    
-        <div className="our-impact lg:w-[568px] flex flex-col items-start gap-8 xs:self-stretch">    
-          <div className="heading-desc flex flex-col content-center items-start gap-4">    
-            <h5 className=" text-sm lg:text-base font-bold">OUR IMPACT</h5>    
-            <p className="text-base font-normal text-neutral-2">    
-              Memberikan akses pendidikan, informasi, dan berbagai pelatihan seluas-luasnya bagi seluruh anak     
-              bangsa guna mempersiapkan pemimpin masa depan Indonesia berdaya saing global.    
-            </p>    
-          </div>    
-          <div className="all-impact flex w-80 flex-col items-start gap-8">    
-            <div className="impact-1 flex flex-col items-start gap-2 self-stretch">    
-              <div className="point flex items-center gap-4 self-stretch">    
-                <div className="icon flex size-9 justify-center items-center rounded-lg bg-brand-red hover:bg-brand-red-hover">    
-                  <MdLocationOn className="text-white size-6" />    
-                </div>    
-                <h1 className=" text-3xl font-bold">34</h1>    
-              </div>    
-              <p className="text-base font-normal text-neutral-2">    
-                Provinsi diseluruh Indonesia    
-              </p>    
-            </div>    
-            <div className="impact-2 flex flex-col items-start gap-2 self-stretch">    
-              <div className="point flex items-center gap-4 self-stretch">    
-                <div className="icon flex size-9 justify-center items-center rounded-lg bg-brand-red hover:bg-brand-red-hover">    
-                  <MdLocationCity className="text-white size-6" />    
-                </div>    
-                <h1 className=" text-3xl font-bold">100+</h1>    
-              </div>    
-              <p className="text-base font-normal text-neutral-2">    
-                Kota diseluruh Indonesia    
-              </p>    
-            </div>    
-            <div className="impact-3 flex flex-col items-start gap-2 self-stretch">    
-              <div className="point flex items-center gap-4 self-stretch">    
-                <div className="icon flex size-9 justify-center items-center rounded-lg bg-brand-red hover:bg-brand-red-hover">    
-                  <MdPeople className="text-white size-6" />    
-                </div>    
-                <h1 className=" text-3xl font-bold">10</h1>    
-              </div>    
-              <p className="text-base font-normal text-neutral-2">    
-                <span className="font-bold">Child Organization</span>{" "}    
-                (IDEpreneur, <br />    
-                IDEpeduli, etc.)    
-              </p>    
-            </div>    
-          </div>    
-        </div>    
-      </div>    
-    </motion.div>    
-  );    
-};    
+    return () => {        
+      window.removeEventListener("scroll", handleScroll);      
+      window.removeEventListener("resize", handleResize); // Clean up the resize listener  
+    };        
+  }, []);        
+  
+  return (        
+    <motion.div        
+      id="home-details"        
+      className="details-section flex justify-center items-center self-stretch"        
+      initial={isMobile ? {} : { opacity: 0, x: -1000 }} // No animation for mobile        
+      animate={isMobile ? { opacity: 1, x: 0 } : (isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: -1000 })} // Conditional animation        
+      transition={{ duration: 1, ease: "easeInOut" }}        
+    >        
+      <div className="details-container flex flex-col lg:flex-row w-full xs:px-4 lg:px-6 lg:w-[1224px] py-12 items-start lg:items-center lg:content-between border-b-[1px] border-neutral-3 gap-6 lg:gap-12">        
+        <div className="our-mission lg:w-[568px] flex flex-col items-start gap-4">        
+          <h5 className="text-sm lg:text-base font-bold">OUR MISSION</h5>        
+          <div className="heading-desc flex flex-col items-start gap-3 self-stretch">        
+            <h2 className="text-xl lg:text-3xl font-bold text-neutral-1">        
+              Melahirkan Manusia Indonesia Berkualitas melalui Ilmu Pengetahuan dan Pengembangan Keterampilan        
+            </h2>        
+            <p className="text-base font-normal text-neutral-2">        
+              IDE Indonesia hadir sebagai jembatan bagi generasi muda di seluruh Indonesia dalam mewujudkan         
+              mimpi dan cita-cita mereka melalui pemberdayaan,         
+              kolaborasi, dan gotong-royong menghadirkan solusi inovatif bagi kemajuan bangsa dan negara.        
+            </p>        
+            <Link        
+              className="text-base font-bold underline decoration-solid text-neutral-1"        
+              to="/about"        
+            >        
+              Learn More        
+            </Link>        
+          </div>        
+        </div>        
+        <hr className="w-full lg:hidden" />        
+        <div className="border-r-[1px] border-neutral-3 h-[402px] lg:block hidden"></div>        
+        <div className="our-impact lg:w-[568px] flex flex-col items-start gap-8">        
+          <div className="heading-desc flex flex-col items-start gap-4">        
+            <h5 className="text-sm lg:text-base font-bold">OUR IMPACT</h5>        
+            <p className="text-base font-normal text-neutral-2">        
+              Memberikan akses pendidikan, informasi, dan berbagai pelatihan seluas-luasnya bagi seluruh anak         
+              bangsa guna mempersiapkan pemimpin masa depan Indonesia berdaya saing global.        
+            </p>        
+          </div>        
+          <div className="all-impact flex flex-col items-start gap-6">        
+            <div className="impact flex flex-col items-start gap-2">        
+              <div className="point flex items-center gap-4">        
+                <div className="icon flex size-9 justify-center items-center rounded-lg bg-brand-red hover:bg-brand-red-hover">        
+                  <MdLocationOn className="text-white size-6" />        
+                </div>        
+                <h1 className="text-2xl lg:text-3xl font-bold">34</h1>        
+              </div>        
+              <p className="text-base font-normal text-neutral-2">        
+                Provinsi di seluruh Indonesia        
+              </p>        
+            </div>        
+            <div className="impact flex flex-col items-start gap-2">        
+              <div className="point flex items-center gap-4">        
+                <div className="icon flex size-9 justify-center items-center rounded-lg bg-brand-red hover:bg-brand-red-hover">        
+                  <MdLocationCity className="text-white size-6" />        
+                </div>        
+                <h1 className="text-2xl lg:text-3xl font-bold">100+</h1>        
+              </div>        
+              <p className="text-base font-normal text-neutral-2">        
+                Kota di seluruh Indonesia        
+              </p>        
+            </div>        
+            <div className="impact flex flex-col items-start gap-2">        
+              <div className="point flex items-center gap-4">        
+                <div className="icon flex size-9 justify-center items-center rounded-lg bg-brand-red hover:bg-brand-red-hover">        
+                  <MdPeople className="text-white size-6" />        
+                </div>        
+                <h1 className="text-2xl lg:text-3xl font-bold">10</h1>        
+              </div>        
+              <p className="text-base font-normal text-neutral-2">        
+                <span className="font-bold">Child Organization</span>{" "}        
+                (IDEpreneur, <br />        
+                IDEpeduli, etc.)        
+              </p>        
+            </div>        
+          </div>        
+        </div>        
+      </div>        
+    </motion.div>        
+  );        
+};        
--- src/components/HomePartner.jsx
+++ src/components/HomePartner.jsx
@@ -1,64 +1,71 @@
-/* eslint-disable no-unused-vars */  
-import React, { useEffect, useState } from "react";    
-import "./HomePartner.css";  
-import Partner1 from "../assets/images/partner-1.png";  
-import Partner2 from "../assets/images/partner-2.png";  
-import Partner3 from "../assets/images/partner-3.png";  
-import Partner4 from "../assets/images/partner-4.png";  
-import { motion } from "framer-motion";     
+/* eslint-disable no-unused-vars */      
+import React, { useEffect, useState } from "react";      
+import "./HomePartner.css";      
+import Partner1 from "../assets/images/partner-1.png";      
+import Partner2 from "../assets/images/partner-2.png";      
+import Partner3 from "../assets/images/partner-3.png";      
+import Partner4 from "../assets/images/partner-4.png";      
+import { motion } from "framer-motion";         
   
-export const HomePartner = () => {  
-  const [isVisible, setIsVisible] = useState(false);        
-  
-  const handleScroll = () => {        
-    const rect = document.getElementById("home-partners").getBoundingClientRect();        
-    if (rect.top < window.innerHeight && rect.bottom >= 0) {        
-      setIsVisible(true);        
-    }        
-  };     
-  
-  useEffect(() => {      
-    window.addEventListener("scroll", handleScroll);      
-    return () => {      
-      window.removeEventListener("scroll", handleScroll);      
+export const HomePartner = () => {      
+  const [isVisible, setIsVisible] = useState(false);      
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);    
+      
+  const handleScroll = () => {            
+    const rect = document.getElementById("home-partners").getBoundingClientRect();            
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {            
+      setIsVisible(true);            
+    }            
+  };         
+      
+  useEffect(() => {            
+    window.addEventListener("scroll", handleScroll);            
+    const handleResize = () => {      
+      setIsMobile(window.innerWidth < 768); // Update mobile state on resize      
     };      
-  }, []);      
-  
-  return (  
-    <motion.div      
-      id="home-partners"      
-      className="details-section flex justify-center items-center self-stretch"      
-      initial={{ opacity: 0, x: 1000 }} // Start from the right     
-      animate={isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: 1000 }} // Move to the original position when visible      
-      transition={{ duration: 1, ease: "easeInOut" }}      
-    >   
-      <div className="our-partner-section flex flex-col items-center self-stretch">  
-        <div className="our-partner-container flex w-full xs:px-6 lg:w-[1224px] flex-col items-start">  
-          <div className="heading flex items-center justify-center gap-6 self-stretch">  
-            <h5 className="text-base font-bold">OUR PARTNERS</h5>  
-            <hr className="border-b-[1px] flex-1 border-neutral-3" />  
-          </div>  
-          <div className="partners flex py-12 justify-center lg:justify-between items-start lg:items-center xs:content-start lg:gap-0 gap-y-6 gap-x-4 self-stretch flex-wrap lg:flex-nowrap">  
-            <div className="partner partner-1 flex content-center items-center">  
-              <img src={Partner1} alt="Partner 1" />  
-            </div>  
-            <div className="partner partner-2 flex content-center items-center">  
-              <img src={Partner2} alt="Partner 2" />  
-            </div>  
-            <div className="partner partner-3 flex content-center items-center">  
-              <img src={Partner3} alt="Partner 3" />  
-            </div>  
-            <div className="partner partner-4 flex content-center items-center">  
-              <img src={Partner4} alt="Partner 4" />  
-            </div>  
-            <div className="partner partner-1 flex content-center items-center">  
-              <img src={Partner1} alt="Partner 1" />  
-            </div>  
-          </div>  
-        </div>  
-      </div>  
-    </motion.div>   
-  );  
-};  
-  
-export default HomePartner;  
+    window.addEventListener("resize", handleResize); // Listen for resize events      
+      
+    return () => {            
+      window.removeEventListener("scroll", handleScroll);          
+      window.removeEventListener("resize", handleResize); // Clean up the resize listener      
+    };            
+  }, []);            
+      
+  return (      
+    <motion.div          
+      id="home-partners"          
+      className="details-section flex justify-center items-center self-stretch"          
+      initial={isMobile ? {} : { opacity: 0, x: 1000 }} // No animation for mobile         
+      animate={isMobile ? { opacity: 1, x: 0 } : (isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: 1000 })} // Conditional animation   
+      transition={{ duration: 1, ease: "easeInOut" }}          
+    >       
+      <div className="our-partner-section flex flex-col items-center self-stretch">      
+        <div className="our-partner-container flex w-full xs:px-6 lg:w-[1224px] flex-col items-start">      
+          <div className="heading flex items-center justify-center gap-6 self-stretch">      
+            <h5 className="text-base font-bold">OUR PARTNERS</h5>      
+            <hr className="border-b-[1px] flex-1 border-neutral-3" />      
+          </div>      
+          <div className="partners flex py-12 justify-center lg:justify-between items-start lg:items-center xs:content-start lg:gap-0 gap-y-6 gap-x-4 self-stretch flex-wrap lg:flex-nowrap">      
+            <div className="partner partner-1 flex content-center items-center">      
+              <img src={Partner1} alt="Partner 1" />      
+            </div>      
+            <div className="partner partner-2 flex content-center items-center">      
+              <img src={Partner2} alt="Partner 2" />      
+            </div>      
+            <div className="partner partner-3 flex content-center items-center">      
+              <img src={Partner3} alt="Partner 3" />      
+            </div>      
+            <div className="partner partner-4 flex content-center items-center">      
+              <img src={Partner4} alt="Partner 4" />      
+            </div>      
+            <div className="partner partner-1 flex content-center items-center">      
+              <img src={Partner1} alt="Partner 1" />      
+            </div>      
+          </div>      
+        </div>      
+      </div>      
+    </motion.div>       
+  );      
+};      
+      
+export default HomePartner;      
