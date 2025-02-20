8128665988abf9ee9fb005eb86183f2e2def832f
WeissCurry <Maulanasykari@gmail.com>
2025-02-20 08:01:12 +0700

    ADD : Social, components, Integrate API

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,9 +1,38 @@
 /* eslint-disable react/prop-types */  
-import React from "react";  
+import React, { useState, useEffect } from "react";  
 import PropTypes from "prop-types";  
 import { SiLinkedin, SiInstagram, SiYoutube, SiFacebook } from "react-icons/si";  
-  
+
 export const Social = ({ hideAt }) => {  
+  const [socialLinks, setSocialLinks] = useState({
+    linkedin: "",
+    instagram: "",
+    youtube: "",
+    facebook: "",
+  });
+
+  useEffect(() => {
+    const fetchCompanyInfo = async () => {
+      try {
+        const response = await fetch("https://server-ideindonesia.ideatecore.com/companyinfo");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setSocialLinks({
+          linkedin: data.data.Linkedin,
+          instagram: data.data.Instagram,
+          youtube: data.data.Youtube,
+          facebook: data.data.Facebook,
+        });
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    };
+
+    fetchCompanyInfo();
+  }, []);
+
   // Determine visibility class based on "hideAt" prop  
   const visibilityClass =  
     hideAt === "mobile"  
@@ -11,13 +40,13 @@ export const Social = ({ hideAt }) => {
       : hideAt === "desktop"  
       ? "flex md:hidden mt-4" // Visible on small screens, hidden on medium and up  
       : "flex"; // Default is visible everywhere  
-  
+
   return (  
     <div  
       className={`social-media items-center gap-3 self-stretch text-white ${visibilityClass}`}  
     >  
       <a  
-        href="https://www.linkedin.com/company/theideindonesia"  
+        href={socialLinks.linkedin}  
         target="_blank"  
         rel="noopener noreferrer"  
         className="flex w-12 h-12 justify-center items-center p-3 rounded-lg bg-brand-red hover:bg-brand-red-hover"  
@@ -25,7 +54,7 @@ export const Social = ({ hideAt }) => {
         <SiLinkedin />  
       </a>  
       <a  
-        href="https://www.instagram.com/theideindonesia"  
+        href={socialLinks.instagram}  
         target="_blank"  
         rel="noopener noreferrer"  
         className="flex w-12 h-12 justify-center items-center p-3 rounded-lg bg-brand-red hover:bg-brand-red-hover"  
@@ -33,7 +62,7 @@ export const Social = ({ hideAt }) => {
         <SiInstagram />  
       </a>  
       <a  
-        href="https://www.youtube.com/@TheIDEIndonesia"  
+        href={socialLinks.youtube}  
         target="_blank"  
         rel="noopener noreferrer"  
         className="flex w-12 h-12 justify-center items-center p-3 rounded-lg bg-brand-red hover:bg-brand-red-hover"  
@@ -41,7 +70,7 @@ export const Social = ({ hideAt }) => {
         <SiYoutube />  
       </a>  
       <a  
-        href="https://www.facebook.com/theideindonesia"  
+        href={socialLinks.facebook}  
         target="_blank"  
         rel="noopener noreferrer"  
         className="flex w-12 h-12 justify-center items-center p-3 rounded-lg bg-brand-red hover:bg-brand-red-hover"  
@@ -51,11 +80,11 @@ export const Social = ({ hideAt }) => {
     </div>  
   );  
 };  
-  
+
 Social.propTypes = {  
   hideAt: PropTypes.oneOf(["mobile", "desktop", ""]),  
 };  
-  
+
 Social.defaultProps = {  
   hideAt: "",  
-};  
+};
