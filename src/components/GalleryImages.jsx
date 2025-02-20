bbf8c35a41d62eeb746a80d330ac6dbd1131bf24
WeissCurry <Maulanasykari@gmail.com>
2025-02-20 07:42:35 +0700

    ADD : Gallery, GalleryImages, Integrate API not done

--- src/components/GalleryImages.jsx
+++ src/components/GalleryImages.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-import gallery1 from "../assets/images/gallery-1.png";
-import gallery2 from "../assets/images/gallery-2.png";
-import gallery3 from "../assets/images/gallery-3.png";
-import gallery4 from "../assets/images/gallery-4.png";
-import gallery5 from "../assets/images/gallery-5.png";
+import React, { useState, useEffect } from "react";
 
 export const GalleryImages = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch("https://server-ideindonesia.ideatecore.com/gallery");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setImages(data.data.map(item => item.image));
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
   return (
     <div className="gallery-images w-full lg:w-[1224px] xs:px-6 flex pb-12 xs:flex-col xs:justify-center items-start lg:items-center gap-6 self-stretch lg:mx-auto border-b-[1px] border-neutral-3">
       <div className="gallery-frame-1 w-full lg:w-[392px] h-[802.98px] lg:h-[824px] flex flex-col items-start gap-6">
-        <img src={gallery1} alt="" />
-        <img src={gallery2} alt="" />
+        {images.slice(0, 2).map((image, index) => (
+          <img key={index} src={image} alt={`gallery-${index + 1}`} />
+        ))}
       </div>
       <div className="gallery-frame-2 w-full lg:w-[392px] h-[824px]">
-        <img src={gallery3} alt="" />
+        {images.length > 2 && <img src={images[2]} alt="gallery-3" />}
       </div>
       <div className="gallery-frame-3 w-full lg:w-[392px] h-[802.98px] lg:h-[824px] flex flex-col items-start gap-6">
-        <img src={gallery4} alt="" />
-        <img src={gallery5} alt="" />
+        {images.slice(3, 5).map((image, index) => (
+          <img key={index + 3} src={image} alt={`gallery-${index + 4}`} />
+        ))}
       </div>
     </div>
   );
