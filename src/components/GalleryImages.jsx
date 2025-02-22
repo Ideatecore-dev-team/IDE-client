898080c14dce81a628bbbb8c3f53589e2dd10050
WeissCurry <Maulanasykari@gmail.com>
2025-02-22 15:09:31 +0700

    REVOKE : gallery, GalleryImages

--- src/components/GalleryImages.jsx
+++ src/components/GalleryImages.jsx
@@ -16,7 +16,7 @@ export const GalleryImages = () => {
   return (
     <div className="gallery-images w-full lg:w-[1224px] xs:px-6 flex pb-12 xs:flex-col xs:justify-center items-start lg:items-center gap-6 self-stretch lg:mx-auto border-b-[1px] border-neutral-3">
       <div className="gallery-frame-1 w-full lg:w-[392px] h-[802.98px] lg:h-[824px] flex flex-col items-start gap-6">
-        {images.slice(3, 5).reverse().map((image, index) => (
+        {images.slice(0, 2).map((image, index) => (
           <img key={index} src={image} alt={`gallery-${index + 1}`} />
         ))}
       </div>
@@ -24,7 +24,7 @@ export const GalleryImages = () => {
         {images.length > 2 && <img src={images[2]} alt="gallery-3" />}
       </div>
       <div className="gallery-frame-3 w-full lg:w-[392px] h-[802.98px] lg:h-[824px] flex flex-col items-start gap-6">
-        {images.slice(0, 2).reverse().map((image, index) => (
+        {images.slice(3, 5).map((image, index) => (
           <img key={index + 3} src={image} alt={`gallery-${index + 4}`} />
         ))}
       </div>
