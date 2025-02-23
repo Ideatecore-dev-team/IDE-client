97eaafb68238617de091ba79426ef5d58a43ced5
WeissCurry <Maulanasykari@gmail.com>
2025-02-23 09:34:11 +0700

    STYLE : AboutUS, AboutUsImpact, lg:p-[0px]

--- src/components/AboutUsImpact.jsx
+++ src/components/AboutUsImpact.jsx
@@ -6,7 +6,7 @@ import { MdPeople } from "react-icons/md";
 
 export const AboutUsImpact = () => {
   return (
-    <div className="our-impact-section flex lg:p-[10px]  lg:mx-auto lg:flex-col xs:justify-center items-center lg:gap-[10px] self-stretch ">
+    <div className="our-impact-section flex lg:p-[0px]  lg:mx-auto lg:flex-col xs:justify-center items-center lg:gap-[10px] self-stretch ">
       <div className="our-impact-container flex flex-col w-full lg:w-[1224px] py-12 items-start  border-b-[1px] border-neutral-3 gap-10 lg:gap-12 xs:items-center">
         {/* <div className="our-impact lg:w-[568px] flex flex-col items-start gap-8 xs:self-stretch"> */}
         <div className="heading-desc flex lg:w-full w-full xs:px-6 lg:justify-between lg:flex-row flex-col xs:content-center items-start xs:gap-4 lg:self-stretch">
