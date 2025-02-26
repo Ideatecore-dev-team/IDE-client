0ec0b48f4db7532b5a9d3968f9ae22421d216417
nhsltn <nhsltndev@gmail.com>
2025-02-26 23:20:48 +0700

    FIX: Responsive Error Handling

--- src/components/ArticleListNotFound.jsx
+++ src/components/ArticleListNotFound.jsx
@@ -4,11 +4,11 @@ import { MdSearch } from "react-icons/md";
 
 export const ArticleListNotFound = () => {
   return (
-    <div className="article-not-found flex flex-col items-center justify-center gap-3 lg:h-[717.08px] w-full">
+    <div className="article-not-found flex flex-col items-center justify-center gap-3 lg:h-[717.08px] w-full xs:pt-12">
       <div className="search-icon flex p-3 justify-center items-center bg-brand-red rounded-lg">
         <MdSearch className="text-white" size={24} />
       </div>
-      <h3 className="text-2xl font-bold">
+      <h3 className="lg:text-2xl text-lg font-bold">
         Artikel yang anda cari tidak tersedia
       </h3>
       <p className="text-base">
